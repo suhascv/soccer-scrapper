f3124bc86ccc78f8f67b054f94db3181cf47cf51
suhascv <suhazcsk@gmail.com>
2021-01-14 13:33:19 -0500

    heroku

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ var puppeteer = require("puppeteer");
 var cheerio = require("cheerio");
 
 async function ssr(url) {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox']});
   const page = await browser.newPage();
   await page.goto(url, { waitUntil: "networkidle0" });
   const html = await page.content(); // serialized HTML of page DOM.
